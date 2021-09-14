f4be87cdfc2803c9be3ea2c2a15d569c60de3d79
Ricardo S. Chao <chaoannricardo@protonmail.com>
2021-09-14 18:14:43 +0800

    [up] added compare model function

--- core/fine_tune.py
+++ core/fine_tune.py
@@ -10,6 +10,24 @@ from .model import LNClassifier
 from .fine_tune_milti_view import update_s, multi_view_learn
 
 
+def compare_models(model_1, model_2):
+    models_differ = 0
+    for key_item_1, key_item_2 in zip(model_1.state_dict().items(), model_2.state_dict().items()):
+        if torch.equal(key_item_1[1], key_item_2[1]):
+            pass
+        else:
+            models_differ += 1
+            if (key_item_1[0] == key_item_2[0]):
+                print('Mismtach found at', key_item_1[0])
+            else:
+                raise Exception
+    if models_differ == 0:
+        print('Models match perfectly! :)')
+        return True
+    else:
+        return False
+
+
 def fine_tune_encoder(opt, encoder, graph_data, seeds):
     n_class = opt['n_class']
     d_es = graph_data.x[0].size(-1)
